18ead0fdc76617807986cf3c57e010f7994e0179
shubhi awasthi13 <awasthishubhi147@gmail.com>
2025-07-08 21:00:13 +0530

    issue fixed

--- LMS_Frontend/src/pages/student/Profile.jsx
+++ LMS_Frontend/src/pages/student/Profile.jsx
@@ -19,7 +19,7 @@ import {
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Loader2 } from "lucide-react";
-import Course from "./course";
+import Course from "./Course";
 import { toast } from "sonner";
 
 function Profile() {
