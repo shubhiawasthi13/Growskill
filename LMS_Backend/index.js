525d816d204dcf40c1d4afa01961e9f3555f8d78
shubhi awasthi13 <awasthishubhi147@gmail.com>
2025-07-09 18:54:55 +0530

    updated

--- LMS_Backend/index.js
+++ LMS_Backend/index.js
@@ -17,7 +17,6 @@ const __dirname = path.resolve();
 console.log(__dirname)
 const app = express();
 const PORT = process.env.PORT;
-app.use("/api/v1/purchase", purchaseRoute);
 
 app.use(express.json());
 app.use(cookieParser());
@@ -32,6 +31,7 @@ app.use(
 app.use("/api/v1/media", mediaRoute);
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/course", courseRoute);
+app.use("/api/v1/purchase", purchaseRoute);
 app.use("/api/v1/progress", courseProgressRoute);
 
 
