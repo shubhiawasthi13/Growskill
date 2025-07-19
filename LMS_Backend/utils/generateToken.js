304d046f37ae508951684f05bfa9bd7d661d9636
shubhi awasthi13 <awasthishubhi147@gmail.com>
2025-07-19 23:16:25 +0530

    updated

--- LMS_Backend/utils/generateToken.js
+++ LMS_Backend/utils/generateToken.js
@@ -8,7 +8,8 @@ export const generateToken = (res, user, message) => {
     .status(200)
     .cookie("token", token, {
       httpOnly: true,
-      sameSite: "strict",
+      secure:true,
+      sameSite: "none",
       maxAge: 24 * 60 * 60 * 1000, // 1 day
     })
     .json({
