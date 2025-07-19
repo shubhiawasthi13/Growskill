81775e9bd053df355a38ecaca2f4e91cb911cfde
shubhi awasthi13 <awasthishubhi147@gmail.com>
2025-07-19 22:01:37 +0530

    updated

--- LMS_Frontend/src/features/api/authApi.js
+++ LMS_Frontend/src/features/api/authApi.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { userLoggedIn } from "../authSlice";
 
-const userAPi = "https://growskill-6gaq.onrender.com/api/v1/user/";
+const userAPi = "https://growskill-6gaq.onrender.com/api/v1/user";
 
 export const authApi = createApi({
   reducerPath: "authApi",
